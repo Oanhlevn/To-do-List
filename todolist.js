1404cf1784b05a587553260fdb5f85fdb9eaec61
Le Oanh <leoanh@H.Oanh>
2019-03-13 13:55:19 +0700

    update Milestone 9

--- todolist.js
+++ todolist.js
@@ -1,44 +1,62 @@
-let MasterTodoList = []; 
+let MasterTodoList = [];
 let addBtn = document.getElementById("addbutton");
-let txtBox = document.getElementById("textbox"); 
-let todoList = document.getElementById("todolist"); 
-let doneOnly = document. getElementById("doneonly"); 
+let txtBox = document.getElementById("textbox");
+let todoList = document.getElementById("todolist");
+let doneOnly = document.getElementById("doneonly");
 
 
-function addList()
-{  
-let todoitem = 
-{ text: txtBox.value , isdone: false}
-  
-MasterTodoList.push(todoitem); 
-   render() ; 
+function addList() {
+    let todoitem =
+        { text: txtBox.value, isdone: false }
+
+    MasterTodoList.push(todoitem);
+    render();
+    txtBox.value=""; 
 }
-let hasbeendone = (i) => { MasterTodoList[i].isdone = ! MasterTodoList[i].isdone; render()}
+let hasbeendone = (i) => { MasterTodoList[i].isdone = !MasterTodoList[i].isdone; render(); }
 
-let render = function()
- {   
- 
-    todoList.innerHTML = MasterTodoList.map((todo, i) => 
-    { if (todo.isdone)
-     { return `<li> <s> ${todo.text} </s> <a href='#' onclick='hasbeendone(${i})'> Mark Undone </a> <a href='#' onclick='remove(${i})'> Remove </a> </li>`;
-     }
-     else {return `<li> ${todo.text} <a href='#' onclick='hasbeendone(${i})'> Mark Done </a> <a href='#' onclick='remove(${i})'> Remove </a> </li>`; 
-     } 
-} ).join(" ");}
+let render = function () {
+    let undonearr;
+    if (checked) {
+        undonearr = MasterTodoList.filter(item => item.isdone == false);
+        todoList.innerHTML = undonearr.map((todo, i) => {
+            return `<li> ${todo.text} <a href='#' onclick='hasbeendone(${i})'> Mark Done </a> </span> <a href='#' onclick='remove(${i})'> X  </a> </li>`;
+        }).join(" ");
+    }
+
+    else {
+        todoList.innerHTML = MasterTodoList.map((todo, i) => {
+            if (todo.isdone) {
+                return `<li> <s> ${todo.text} </s> <a href='#' onclick='hasbeendone(${i})'> Mark Undone </a> <a href='#' onclick='remove(${i})'> Remove </a> </li>`;
+            }
+            else {
+                return `<li> ${todo.text} <a href='#' onclick='hasbeendone(${i})'> Mark Done </a> <a href='#' onclick='remove(${i})'> X </a> </li>`;
+            }
+        }).join(" ");
+    }
+}
 
 
-function remove(i) { 
-    MasterTodoList.splice(i,1);
-    render();  }
+function remove(i) {
+    MasterTodoList.splice(i, 1);
+    render();
+}
+let checked = false;
+
+function showundone() {
+    if (doneOnly.checked) { checked = true; render(); }
+    else { checked = false; render(); }
+}
+
+addBtn.addEventListener("click", addList);
+doneOnly.addEventListener("change", showundone);
+
+//localStorage.setItem('Masterlist', MasterTodoList); 
 
 
 
-addBtn.addEventListener("click",addList); 
-//doneOnly.addEventListener ("checked", showundone); 
 
-MasterTodoList = [{text: 'orange', isdone: false}, {text:'banana', isdone: true}];
 
-console.log(MasterTodoList); 
 
 
 
